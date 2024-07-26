5279791e258139f09525c1af1e53df3be3d7915b
dl10yr <tkzw04202166@gmail.com>
2024-07-26 23:02:02 +0900

    home and preview

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,9 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import React, { useContext } from "react";
+import { sourceFilesContext } from "~/root";
+
+import styles from "~/styles/_index.scss?url";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,32 +12,95 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
 export default function Index() {
+  const { sourceFilesData, setSourceFilesData } =
+    useContext(sourceFilesContext);
+
+  const selectedYamlName =
+    sourceFilesData.yaml !== undefined ? sourceFilesData.yaml.name : "";
+  const faceImageName = sourceFilesData.faceImage?.name;
+
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event?.target?.files;
+    if (files === null) return;
+    const file = files[0];
+    if (file !== null) {
+      setSourceFilesData({ ...sourceFilesData, yaml: file });
+    }
+  };
+
+  const faceImageFileInputRef = React.useRef<HTMLInputElement>(null);
+  const handleFaceImageButtonClick = () => {
+    faceImageFileInputRef.current?.click();
+  };
+
+  const handleFaceImageFileChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const files = event?.target?.files;
+    if (files === null) return;
+    const file = files[0];
+    if (file !== null) {
+      setSourceFilesData({ ...sourceFilesData, faceImage: file });
+    }
+  };
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix (SPA Mode)</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/guides/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="home">
+      <div className="home-header">
+        <h1 className="title">履歴書ジェネレーター</h1>
+        <p className="description">
+          ※全ての処理はローカル（端末側）で完結するので安全に利用できます
+        </p>
+        <p className="description">
+          yamlのサンプルはここからダウンロードしてください
+        </p>
+      </div>
+
+      <div className="step">
+        <h2 className="step-title">1. yamlを参照</h2>
+        <p>選択されたファイル: {selectedYamlName}</p>
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={(e) => handleFileChange(e)}
+          style={{ display: "none" }}
+          accept=".yaml,.yml"
+        />
+        <button onClick={handleButtonClick}>yamlファイルを参照</button>
+      </div>
+      <div className="step">
+        <h2 className="step-title">2. 顔写真を参照</h2>
+        <p>選択されたファイル: {faceImageName}</p>
+        <input
+          type="file"
+          ref={faceImageFileInputRef}
+          onChange={(e) => handleFaceImageFileChange(e)}
+          style={{ display: "none" }}
+          accept=".jpg,.png"
+        />
+        <button onClick={handleFaceImageButtonClick}>
+          顔写真ファイルを参照
+        </button>
+      </div>
+      <div className="step">
+        <h2 className="step-title">3. 履歴書を作成する</h2>
+        {sourceFilesData.yaml !== undefined &&
+        sourceFilesData.faceImage !== undefined ? (
+          <Link aria-label="作成する" to="/preview">
+            作成する
+          </Link>
+        ) : (
+          <button disabled>作成する</button>
+        )}
+      </div>
     </div>
   );
 }
--- app/routes/preview.tsx
+++ app/routes/preview.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { useContext, useEffect } from "react";
 import { Education, links as educationLinks } from "~/components/Education";
 import {
   HeaderAndPersonalInfo,
@@ -11,6 +12,9 @@ import {
   links as personalInfoLinks,
 } from "~/components/personalinfo";
 import { Requests, links as requestsLinks } from "~/components/Requests";
+import { sourceFilesContext } from "~/root";
+
+import yaml from "js-yaml";
 
 import styles from "~/styles/preview.css?url";
 
@@ -29,6 +33,52 @@ export const links: LinksFunction = () => [
 ];
 
 export default function Preview() {
+  const { sourceFilesData, setSourceFilesData } =
+    useContext(sourceFilesContext);
+
+  // Function to parse YAML
+  function parseYaml(yamlString: string) {
+    try {
+      const parsedData = yaml.load(yamlString);
+      return parsedData;
+    } catch (error) {
+      console.error("Error parsing YAML:", error);
+      return null;
+    }
+  }
+
+  // Function to read YAML file
+  async function readYamlFile(): Promise<string> {
+    const file = sourceFilesData.yaml;
+    console.log(sourceFilesData);
+    if (file === undefined) {
+      window.alert("yamlがないので作成できません");
+      window.location.href = "/";
+      throw Error("");
+    }
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const yamlString = event.target?.result as string;
+        resolve(yamlString);
+      };
+      reader.onerror = (event) => {
+        reject(new Error("Failed to read YAML file."));
+      };
+      reader.readAsText(file);
+    });
+  }
+
+  async function loadYamlFile() {
+    const yamlString = await readYamlFile();
+    const parsedData = parseYaml(yamlString);
+    console.log(parsedData);
+  }
+
+  useEffect(() => {
+    loadYamlFile();
+  }, []);
+
   return (
     <div className="resume">
       <div className="left">
