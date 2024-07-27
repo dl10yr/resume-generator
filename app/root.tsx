3bb31fa4de5ec6d23e1da1c1f8e8b38d308c4e54
dl10yr <tkzw04202166@gmail.com>
2024-07-27 10:06:42 +0900

    route.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -6,22 +6,62 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { createContext, useState } from "react";
+
+type SourceFilesData = {
+  yaml: File | undefined;
+  faceImage: File | undefined;
+};
+
+type SourceFilesContextData = {
+  sourceFilesData: SourceFilesData;
+  setSourceFilesData: (newData: SourceFilesData) => void;
+};
+
+interface SourceFilesProviderProps {
+  children: React.ReactNode;
+}
+
+export const sourceFilesContext = createContext<SourceFilesContextData>({
+  sourceFilesData: {
+    yaml: undefined,
+    faceImage: undefined,
+  },
+  setSourceFilesData: () => {},
+});
+
+const SourceFilesProvider = (props: SourceFilesProviderProps) => {
+  const [sourceFilesData, setSourceFilesData] = useState<SourceFilesData>({
+    yaml: undefined,
+    faceImage: undefined,
+  });
+
+  return (
+    <sourceFilesContext.Provider
+      value={{ sourceFilesData, setSourceFilesData }}
+    >
+      {props.children}
+    </sourceFilesContext.Provider>
+  );
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <SourceFilesProvider>
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    </SourceFilesProvider>
   );
 }
 
