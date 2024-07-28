8f0db55144b43fa9d0c60158975b4eeca75bf416
dl10yr <tkzw04202166@gmail.com>
2024-07-28 10:05:38 +0900

    preview

--- app/routes/preview.tsx
+++ app/routes/preview.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Education, links as educationLinks } from "~/components/Education";
 import {
   HeaderAndPersonalInfo,
@@ -13,10 +13,12 @@ import {
 } from "~/components/personalinfo";
 import { Requests, links as requestsLinks } from "~/components/Requests";
 import { sourceFilesContext } from "~/root";
-
-import yaml from "js-yaml";
-
 import styles from "~/styles/preview.css?url";
+import { parseYaml, ResumeInfo } from "~/lib/resume";
+import {
+  CommuteAndDependent,
+  links as commuteAndDependentLinks,
+} from "~/components/CommuteAndDependent";
 
 export const meta: MetaFunction = () => {
   return [{ title: "resume" }];
@@ -29,32 +31,21 @@ export const links: LinksFunction = () => [
   ...licensesLinks(),
   ...motivationLinks(),
   ...requestsLinks(),
+  ...commuteAndDependentLinks(),
   { rel: "stylesheet", href: styles },
 ];
 
 export default function Preview() {
-  const { sourceFilesData, setSourceFilesData } =
-    useContext(sourceFilesContext);
-
-  // Function to parse YAML
-  function parseYaml(yamlString: string) {
-    try {
-      const parsedData = yaml.load(yamlString);
-      return parsedData;
-    } catch (error) {
-      console.error("Error parsing YAML:", error);
-      return null;
-    }
-  }
+  const { sourceFilesData } = useContext(sourceFilesContext);
+  const [resumeInfo, setResumeInfo] = useState<ResumeInfo | null>(null);
 
   // Function to read YAML file
-  async function readYamlFile(): Promise<string> {
+  async function readYamlFile(): Promise<string | null> {
     const file = sourceFilesData.yaml;
-    console.log(sourceFilesData);
     if (file === undefined) {
-      window.alert("yamlがないので作成できません");
+      window.alert("yamlが参照されていないので作成できません");
       window.location.href = "/";
-      throw Error("");
+      return null;
     }
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
@@ -62,7 +53,7 @@ export default function Preview() {
         const yamlString = event.target?.result as string;
         resolve(yamlString);
       };
-      reader.onerror = (event) => {
+      reader.onerror = () => {
         reject(new Error("Failed to read YAML file."));
       };
       reader.readAsText(file);
@@ -71,28 +62,54 @@ export default function Preview() {
 
   async function loadYamlFile() {
     const yamlString = await readYamlFile();
+    if (yamlString === null) {
+      return;
+    }
     const parsedData = parseYaml(yamlString);
-    console.log(parsedData);
+    setResumeInfo(parsedData);
   }
 
   useEffect(() => {
     loadYamlFile();
   }, []);
 
+  if (resumeInfo === null) {
+    return <div>Loading...</div>;
+  }
+
+  const requestsVisible =
+    resumeInfo.requests !== null &&
+    resumeInfo.requests.replaceAll("\n", "") !== "";
+
   return (
     <div className="resume">
       <div className="left">
-        <HeaderAndPersonalInfo />
-        <PersonalInfo name="" age={13} email="" />
-        <Education />
+        <HeaderAndPersonalInfo
+          name={resumeInfo.name}
+          photofile={sourceFilesData.faceImage}
+        />
+        <PersonalInfo
+          birthdate={resumeInfo.birthdate}
+          sex={resumeInfo.sex}
+          address1={resumeInfo.address1}
+          address2={resumeInfo.address2}
+          phone1={resumeInfo.phone1}
+          phone2={resumeInfo.phone2}
+          email1={resumeInfo.email1}
+          email2={resumeInfo.email2}
+        />
+        <Education educationWork={resumeInfo.educationWork} />
       </div>
 
       <div className="right">
-        <Licenses />
+        <Licenses licenses={resumeInfo.licenses} />
         <div className="motivation-requests">
-          <Motivation />
-          <Requests />
+          <Motivation motivation={resumeInfo.motivation} />
+          {requestsVisible && <Requests requests={resumeInfo.requests} />}
         </div>
+        <CommuteAndDependent
+          commuteAndDependent={resumeInfo.commuteAndDependent}
+        />
       </div>
     </div>
   );
