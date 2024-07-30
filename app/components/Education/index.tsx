a63e9c111e0c117df225f4dfb3ffcfad6d6d55ba
dl10yr <tkzw04202166@gmail.com>
2024-07-30 17:46:03 +0900

    add Education

--- app/components/Education/index.tsx
+++ app/components/Education/index.tsx
@@ -2,10 +2,25 @@ import { LinksFunction } from "@remix-run/node";
 import React from "react";
 
 import styles from "./styles.css?url";
+import { ResumeTableData } from "~/lib/resume";
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles, type: "text/css" },
+];
 
-export const Education: React.FC = () => {
+export const Education: React.FC<{ educationWork: ResumeTableData }> = ({
+  educationWork,
+}) => {
+  const numOfDisplayRows = 11;
+  const displayEducationWork = Array(numOfDisplayRows)
+    .fill({
+      date: "",
+      name: "",
+    })
+    .map((_, index) => {
+      return educationWork[index] || { date: "", name: "" };
+    });
+
   return (
     <div className="education">
       <table>
@@ -14,51 +29,16 @@ export const Education: React.FC = () => {
           <th>月</th>
           <th>学歴・職歴</th>
         </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
+        {displayEducationWork.map((data) => {
+          const [year, month] = data.date.split("/");
+          return (
+            <tr key={data.date}>
+              <td>{year}</td>
+              <td>{month}</td>
+              <td>{data.name}</td>
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
