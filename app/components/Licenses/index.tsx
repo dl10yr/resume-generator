d8c430ad129145b08384857e98597424cd8eca70
dl10yr <tkzw04202166@gmail.com>
2024-07-31 17:48:06 +0900

    Licenses

--- app/components/Licenses/index.tsx
+++ app/components/Licenses/index.tsx
@@ -2,10 +2,25 @@ import { LinksFunction } from "@remix-run/node";
 import React from "react";
 
 import styles from "./styles.css?url";
+import { ResumeTableData } from "~/lib/resume";
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles, type: "text/css" },
+];
 
-export const Licenses: React.FC = () => {
+export const Licenses: React.FC<{ licenses: ResumeTableData }> = ({
+  licenses,
+}) => {
+  const numOfDisplayRows = 10;
+  const displayLicenses = Array(numOfDisplayRows)
+    .fill({
+      date: "",
+      name: "",
+    })
+    .map((_, index) => {
+      return licenses[index] || { date: "", name: "" };
+    });
+
   return (
     <div className="licenses">
       <table>
@@ -14,46 +29,16 @@ export const Licenses: React.FC = () => {
           <th>月</th>
           <th>免許・資格</th>
         </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
+        {displayLicenses.map((data) => {
+          const [year, month] = data.date.split("/");
+          return (
+            <tr key={data.date}>
+              <td>{year}</td>
+              <td>{month}</td>
+              <td>{data.name}</td>
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
