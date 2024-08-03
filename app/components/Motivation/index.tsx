fbf0c14848e64ea36ee6e0bb691c1eb5c02d7402
dl10yr <tkzw04202166@gmail.com>
2024-08-03 10:18:52 +0900

    add Motivation

--- app/components/Motivation/index.tsx
+++ app/components/Motivation/index.tsx
@@ -2,13 +2,29 @@ import { LinksFunction } from "@remix-run/node";
 import React from "react";
 
 import styles from "./styles.css?url";
+import linkifyStr from "linkify-string";
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles, type: "text/css" },
+];
 
-export const Motivation: React.FC = () => {
+export const Motivation: React.FC<{ motivation: string }> = ({
+  motivation,
+}) => {
+  const linkifiedString = {
+    __html: linkifyStr(motivation, {
+      className: "motivation-content-link",
+      target: "_blank",
+      rel: "noopener noreferrer",
+    }),
+  };
   return (
     <div className="motivation">
-      <div>志望動機</div>
+      <div className="motivation-title">志望動機</div>
+      <div
+        className="motivation-content"
+        dangerouslySetInnerHTML={linkifiedString}
+      />
     </div>
   );
 };
