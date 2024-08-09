a259f0f13c892456a05dac5a3b9edee5ee9fb3de
dl10yr <tkzw04202166@gmail.com>
2024-08-09 23:16:08 +0900

    add requests

--- app/components/Requests/index.tsx
+++ app/components/Requests/index.tsx
@@ -3,14 +3,17 @@ import React from "react";
 
 import styles from "./styles.css?url";
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles, type: "text/css" },
+];
 
-export const Requests: React.FC = () => {
+export const Requests: React.FC<{ requests: string }> = ({ requests }) => {
   return (
     <div className="requests">
       <div>
         本人希望記入欄（特に給料、職種、勤務時間、勤務地、その他についての希望などがあれば記入）
       </div>
+      <div>{requests}</div>
     </div>
   );
 };
